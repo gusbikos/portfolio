8fa161f5514f0c3a0d9a2d012f7eaa8a22df7017
Gus Bikos <gusbikos@gmail.com>
2021-06-30 22:12:05 -0400

    Added navbar functionality for when browser page becomes smaller

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ import { useState } from 'react'
 
 const NavBar = () => {
     const [navBar, setNavBar] = useState(false)
-    const [toggleMenu, setToggleMenu] = useState(false)
+    const [clickMenu, setClickMenu] = useState(false)
 
     const changeNavBarBackground = () => {
         if(window.scrollY >= 20) {
@@ -15,7 +15,7 @@ const NavBar = () => {
     }
 
     const handleMenuClick = () => {
-    
+        setClickMenu(!clickMenu)
     }
 
     window.addEventListener('scroll', changeNavBarBackground)
@@ -59,8 +59,40 @@ const NavBar = () => {
                 {/* <div>
                     <SocialIcon url='https://github.com/gusbikos' className='' target="_blank"/>
                 </div> */}
-                <div className="menu-btn">
-                    <i className="fas fa-bars"></i>
+                <div className="menu-btn" onClick={handleMenuClick}>
+                    <i className="fas fa-bars"/>
+                        <ul className={ clickMenu ? 'navbar menu active' : 'navbar menu' }>
+                            <li>
+                                <NavLink to='/portfolio'>
+                                    Homepage
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/aboutme'>
+                                    About Me
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/blogs'>
+                                    Blogs
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/contact'>
+                                    Reach out to me!
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/projects'>
+                                    Projects
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/skills'>
+                                    Technical Skills
+                                </NavLink>
+                            </li>
+                        </ul>
                 </div>
             </div>
         </nav>    
