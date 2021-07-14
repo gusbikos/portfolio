d538b97644bb717446103b46a3e6363c7984ff3c
Gus Bikos <gusbikos@gmail.com>
2021-07-13 20:55:58 -0400

    added email feature in contact section using emailjs

--- src/HomePage.js
+++ src/HomePage.js
@@ -10,7 +10,10 @@ const HomePage = () => {
                     <div className="home-content">
                         <div className="text-1">Hello, my name is </div>
                         <div className="text-2">Gus Bikos </div>
-                        <div className="text-3">And I'm a <span>Developer</span></div>
+                        <div className="text-3">
+                            <p> <TypeWriter /> </p>
+                        </div>
+                        {/* <div className="text-3">And I'm a <span>Full Stack Web Developer</span></div> */}
                         <a href={Resume}>Hire me</a>
                     </div>
                 </div>
