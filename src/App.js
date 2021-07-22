cc30afdc9d56802f8fd008d571e12aa92c1d26a7
Gus Bikos <gusbikos@gmail.com>
2021-07-21 20:11:46 -0400

    n n
    nbnb

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css'
+import { Route, Switch } from 'react-router-dom'
+import HomePage from './HomePage'
+import AboutMe from './Aboutme'
+import Blogs from './Blogs'
+import Contact from './Contact'
+import Footer from './Footer'
+import NavBar from './NavBar'
+import Projects from './Projects'
+import Skills from './Skills'
+import Services from './Services'
+
+const  App = () => {
+  return (
+    <div className="App">
+      <NavBar/>
+        <div className="portfolio">
+          <HomePage/>
+          <AboutMe/>
+          <Services/>
+          <Skills/>
+          <Projects/>
+          <Contact/>
+          <Footer/>
+        </div>
+    </div>
+  )
+}
+
+export default App
